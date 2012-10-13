ed920ad54b400c284031c4ee18cf384abbc345a8
backyardjsdev <backyardjsdev@ backyardjsdev.com>
2012-10-13 16:02:17 -0700

    added stock animation

--- bean.js
+++ bean.js
@@ -5,7 +5,8 @@ $(function(){
                 "images/cup2.png",
                 "images/heart1.png",
                 "images/heart2.png",
-                "images/heart3.png"]
+                "images/heart3.png"];
+  var stockImage = "images/stock.png";
   $('img').bind('dragstart', function(e) { e.preventDefault(); });
   var socket = io.connect('http://abovesobelow:8800');
 
@@ -16,13 +17,21 @@ $(function(){
 
   socket.on('bean_emit', function(data) {
     var len = images.length;
-    $('body').append('<img id="'+ data.id +'" class="bean" src="'+ images[Math.floor(Math.random()*images.length)] +'"/>');
+    $('body').append('<img id="'+ data.id +'" class="bean" src="'+ images[Math.floor(Math.random()*images.length)] +'"/>')
+             .append('<img id="stock' + data.id +'" class="stock" src="'+ stockImage+'/>");
     var bean = $('img#'+ data.id);
+    var stock = $('img#stock'+ data.id);
     var posX = data.x - 60;
     var posY = data.y - 60;
     bean.css({ top: posY, 
               left: posX,
             zIndex: data.id });
+    stock.css({ top: posY, 
+              left: posX,
+            zIndex: data.id }).hide();
+    bean.fadeOut(3000, function() {
+      stock.fadeIn(500);
+    });
 
   });
 });
