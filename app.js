58d6f098db14e41bc30293cd0c67350541797616
Christopher Toledo <uberscientist@gmail.com>
2012-10-13 15:28:41 -0700

    yay works

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var count = 0;
 
 io.sockets.on('connection', function(socket) {
   socket.on('place_bean', function(data) {
+    console.log(data);
     count++;
     data.id = count;
     socket.emit('bean_emit', data);
--- bean.js
+++ bean.js
@@ -1,13 +1,20 @@
 $(function(){
-  var socket = io.connect('http://localhost:8080');
+  $('img').bind('dragstart', function(e) { e.preventDefault(); });
+  var socket = io.connect('http://localhost:8000');
 
   $(window).click(function(e) {
     var data = {x: e.clientX, y: e.clientY};
     socket.emit('place_bean', data);
-  }
+  });
 
   socket.on('bean_emit', function(data) {
-    $('body').append('<img id="'+ data.id +'" class="bean" src="images/bean1.jpg" />');
-    $('img#'+ data.id).css({ top: data.Y, left: data.X });
-  }
+    console.log(data);
+    $('body').append('<img id="'+ data.id +'" class="bean" src="images/bean1.png" />');
+    var bean = $('img#'+ data.id);
+    bean.css(
+    { top: data.y, 
+      left: data.x + 'px',
+      zIndex: data.id });
+
+  });
 });
