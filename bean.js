73f9f2038cfe430821497f05d32d85b8a6070fb7
backyardjsdev <backyardjsdev@ backyardjsdev.com>
2012-10-13 15:54:02 -0700

    randomized seeds

--- bean.js
+++ bean.js
@@ -1,4 +1,11 @@
 $(function(){
+  var images = ["images/bean1.png", 
+                "images/bean2.png",
+                "images/cup1.png",
+                "images/cup2.png",
+                "images/heart1.png",
+                "images/heart2.png",
+                "images/heart3.png"]
   $('img').bind('dragstart', function(e) { e.preventDefault(); });
   var socket = io.connect('http://localhost:8000');
 
@@ -8,7 +15,8 @@ $(function(){
   });
 
   socket.on('bean_emit', function(data) {
-    $('body').append('<img id="'+ data.id +'" class="bean" src="images/bean1.png" />');
+    var len = images.length;
+    $('body').append('<img id="'+ data.id +'" class="bean" src="'+ images[Math.floor(Math.random()*images.length)] +'"/>');
     var bean = $('img#'+ data.id);
     var posX = data.x - 60;
     var posY = data.y - 60;
